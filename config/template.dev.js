ebaa6409cbe1da13b93c9c5fcb0f4804cd5f9298
iDmitriy-dev <dmitriy.p86@ya.ru>
2019-11-24 23:45:21 +0600

    backend survey creation with mongoose

--- config/template.dev.js
+++ config/template.dev.js
@@ -1,10 +1,14 @@
-// !!!!!! template.dev.js
+// template.dev.js
 
 module.exports = {
-   googleClientID: "-.apps..com",
-   googleClientSecret: "",
-   mongoURI: "mongodb+srv://:@-.mongodb.net/test?retryWrites=true&w=majority",
-   cookieKey: "",
-   stripePublishableKey: "",
-   stripeSecretKey: ""
+   googleClientID: "123456789-add_your_client_id.apps.googleusercontent.com",
+   googleClientSecret: "3_your_client_secret_ASDFGH123",
+   mongoURI: "mongodb+srv://your_name:your_password@your_domain.mongodb.net/test?retryWrites=true&w=majority",
+   cookieKey: "somerandomlettersforcookie",
+   stripePublishableKey: "pk_test_qwer_1234_your_publishable_key",
+   stripeSecretKey: "sk_test_your+stripe_secret_key",
+   sendGridKey: "SG.8000000000000000.YEs=your_sendgrid_key-booboo-2QnJ_C0FI",
+   redirectDomain: 'http://localhost:3000'
 }
+
+//rename this file to .dev.js and add your keys for development
