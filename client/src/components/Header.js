0e9a2a310c511e19eec5e101a7bd72df381baf6b
iDmitriy-dev <dmitriy.p86@ya.ru>
2019-11-26 22:17:11 +0600

    client side survey

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -39,7 +39,7 @@ class Header extends Component {
                >
                   Emaily
                </Link>
-               <ul className='right hide-on-med-and-down'>
+               <ul className='right'>
                   {this.renderContent()}
                </ul>
             </div>
