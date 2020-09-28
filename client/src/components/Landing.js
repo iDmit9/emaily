28e2fa0ed5fa257ec5c4106c6b8ec4c2dbbfcfb1
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2020-09-28 19:36:42 +0600

    redirected-to-surveys-if-auth

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useSelector } from 'react-redux';
 
-const Landing = () => {
+const Landing = (props) => {
+   const auth = useSelector(state => {
+      return state.auth;
+   });
+   
+   if (auth) {
+      props.history.push('/surveys');
+   }
+
    return (
       <div className="container text-center">
          <h1>Emaily!</h1>
