93b4ed2e205eb5f8faa49bc35919aa726421f480
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2020-10-02 23:27:39 +0600

    updated-to-sendgrid-v-7

--- config/template.dev.js
+++ config/template.dev.js
@@ -8,7 +8,8 @@ module.exports = {
    stripePublishableKey: "pk_test_qwer_1234_your_publishable_key",
    stripeSecretKey: "sk_test_your+stripe_secret_key",
    sendGridKey: "SG.8000000000000000.YEs=your_sendgrid_key-booboo-2QnJ_C0FI",
-   redirectDomain: 'http://localhost:3000'
+   redirectDomain: 'http://localhost:3000',
+   emailFrom: 'no-reply@emaily.com' //here you should provide a verified email in order to comply with anti-spam laws.
 }
 
 //rename this file to .dev.js and add your keys for development
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,57 +1,30 @@
-const sendgrid = require('sendgrid');
-const helper = sendgrid.mail;
 const keys = require('../config/keys');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(keys.sendGridKey);
 
-class Mailer extends helper.Mail {
+class Mailer {
    constructor({ subject, recipients }, content) {
-      super();
 
-      this.sgApi = sendgrid(keys.sendGridKey);
+      this.msg = {
+         to: recipients.map(({ email }) =>  email),
+         from: keys.emailFrom, 
+         subject: subject,
+         html: content,
 
-      this.from_email = new helper.Email('no-reply@emaily.com');
-      this.subject = subject;
-      this.body = new helper.Content('text/html', content);
-      this.recipients = this.formatAddresses(recipients);
-
-      this.addContent(this.body);
-      this.addClickTracking();
-      this.addRecipients();
-   }
-
-   formatAddresses(recipients) {
-      return recipients.map(({ email }) => {
-         return new helper.Email(email);
-      });
-   }
-
-   addClickTracking() {
-      const trackingSettings = new helper.TrackingSettings();
-      const clickTracking = new helper.ClickTracking(true, true);
-
-      trackingSettings.setClickTracking(clickTracking);
-      this.addTrackingSettings(trackingSettings);
-   }
-
-   addRecipients() {
-      const personalize = new helper.Personalization();
-
-      this.recipients.forEach(recipient => {
-         personalize.addTo(recipient);
-      });
-      this.addPersonalization(personalize);
+         tracking_settings: {
+            click_tracking: {
+               enable: true, 
+               enable_text: true
+            }, 
+         },
+      };
    }
 
    async send() {
-      const request = this.sgApi.emptyRequest({
-         method: 'POST',
-         path: '/v3/mail/send',
-         body: this.toJSON()
-      });
-
-      const response = await this.sgApi.API(request);
+      const response = await sgMail.sendMultiple(this.msg);
       return response;
    }
-
+   
 }
 
 module.exports = Mailer;
